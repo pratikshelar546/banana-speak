d40a272e042e24e63f7425f250332cb3546ad32d
pratikshelar546 <66733074+pratikshelar546@users.noreply.github.com>
2022-06-29 18:44:48 +0530

    javascript done

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-var btnprimary = document.querySelector("#btn-primary");
 var txtarea = document.querySelector("#txt-area");
+var btnprimary = document.querySelector("#btn-primary");
 var outputdiv = document.querySelector("#output");
 
-function clickhander(){
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-    outputdiv.innerHTML = "sskss " + txtarea.value;
+
+function getTranslationURL(input){
+    return serverURL + "?" +"text=" + input
+}
+
+function errorHandler(error){
+    console.log("some error occured" + error);
+    alert("somthing went wrong with server please check again after some time");
+}
+
+
+
+
+function clickhander(){
+    var inputText = txtarea.value;
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translateText = json.contents.translated;
+     
+        outputdiv.innerHTML = translateText;
+        
+    })
+    .catch(errorHandler)
 };
 btnprimary.addEventListener("click", clickhander);
